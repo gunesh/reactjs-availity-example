ec578628d04fcdc686436ac0c32ace099f6d6c70
gunesh <gunesh26@gmail.com>
2020-09-05 20:39:56 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import Example from "./Example";
 import "./style.css";
-import { Form, Field } from '@availity/form';
-import * as yup from 'yup';
-import { Button } from 'reactstrap';
+import { Form, Field } from "@availity/form";
+import * as yup from "yup";
+import { Button } from "reactstrap";
 
 class App extends Component {
   render() {
@@ -12,15 +12,17 @@ class App extends Component {
       <div style={{ padding: 100 }}>
         <Form
           initialValues={{
-            hello: ""
+            email: ""
           }}
           onSubmit={values => alert(JSON.stringify(values))}
           validationSchema={yup.object().shape({
             hello: yup.string().required()
           })}
         >
-          <Field name="hello" type="text" label="Hello" />
-          <Button type="submit" color="primary">Submit</Button>
+          <Field name="email" labelClass="required" type="text" label="Email" />
+          <Button type="submit" color="primary">
+            Submit
+          </Button>
         </Form>
       </div>
     );
