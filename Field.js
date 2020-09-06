eb7edeadceaf8ed81e85c71331c47ddc36806608
gunesh <gunesh26@gmail.com>
2020-09-06 15:27:31 +0530

    2 files modified

--- Field.js
+++ Field.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import Example from "./Example";
 import "./style.css";
 import {
   Form,
@@ -12,28 +11,25 @@ import {
   RadioGroup,
   Radio
 } from "@availity/form";
-import { Phone, validatePhone } from "@availity/phone";
-import DateField from "@availity/date";
-import { avDate } from "@availity/yup";
-import { SelectField } from "@availity/select";
+
 import * as yup from "yup";
 import { Button, Label } from "reactstrap";
-const Field= ({}) => {
+const Field = ({}) => {
   return (
-      <Form
-  initialValues={{
-    hello: '',
-  }}
-  onSubmit={values => alert(JSON.stringify(values))}
-  validationSchema={yup.object({
-    hello: yup.string().isRequired(true)
-  })}
->
-  <Field name="hello" label="Greeting" />
-  <Button type="submit" color="primary">Submit</Button>
-</Form>
-    );
-}
+    <Form
+      initialValues={{
+        hello: ""
+      }}
+      onSubmit={values => alert(JSON.stringify(values))}
+      validationSchema={yup.object({
+        
+      })}
+    >
+      <Field name="hello" label="Greeting" />
+      <Button type="submit" color="primary">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 export default Field;
-
-
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import Example from "./Example";
+import Field from "./Field";
 import "./style.css";
 import {
   Form,
@@ -13,84 +13,14 @@ import {
   Radio
 } from "@availity/form";
 import { Phone, validatePhone } from "@availity/phone";
-import DateField from "@availity/date";
-import { avDate } from "@availity/yup";
-import { SelectField } from "@availity/select";
-import * as yup from "yup";
+
 import { Button, Label } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
       <div style={{ padding: 100 }}>
-        <Form
-          initialValues={{
-            email: "",
-            phone: "",
-            test_radio: "",
-            test_checkbox: "",
-            dateOfService: "",
-            justTheInput: ""
-          }}
-          onSubmit={values => alert(JSON.stringify(values))}
-          validationSchema={yup.object({
-            email: yup.string().required("Email required"),
-            pswrd: yup.string().required("Password required"),
-            hello: yup.string().required("hello required")
-          })}
-        >
-          <Field name="email" labelClass="required" type="text" label="Email" />
-
-          <Phone
-            name="phone"
-            label="Phone"
-            country="US"
-            showExtension={true}
-            phoneColProps={{ xs: { size: 9 } }}
-            extProps={{
-              name: "ext",
-              label: "Ext.",
-              extColProps: {
-                xs: { size: 3 }
-              }
-            }}
-          />
-
-          <CheckboxGroup name="test_checkbox" label="Checkbox Group">
-            <Checkbox label="Check One" value="uno" />
-            <Checkbox label="Check Two" value="dos" />
-            <Checkbox label="Check Three" value="tres" />
-          </CheckboxGroup>
-
-          <RadioGroup name="heltest_radio" label="Radio Group">
-            <Radio label="Radio One" value="uno" />
-            <Radio label="Radio Two" value="dos" />
-            <Radio label="Radio Three" value="tres" />
-          </RadioGroup>
-
-          <DateField
-            label="Date of Service"
-            id="dateOfService"
-            name="dateOfService"
-            min={{ value: 7, units: "day" }}
-            max={{ value: 7, units: "day" }}
-          />
-
-          <SelectField
-            label="Just The Input"
-            name="justTheInput"
-            isMulti={false}
-            options={[
-              { label: "Option 1", value: 1 },
-              { label: "Option 2", value: 2 },
-              { label: "Option 3", value: 3 }
-            ]}
-          />
-
-          <Button type="submit" color="primary">
-            Submit
-          </Button>
-        </Form>
+       <Field />
       </div>
     );
   }
